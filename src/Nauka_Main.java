4f320f639e6d18f6a83bf5be25607b1695e85420
Jacci19 <jacek205@wp.pl>
2019-02-12 10:51:27 +0100

    01_initial commit

---
+++ src/Nauka_Main.java
@@ -0,0 +1,19 @@
+public class Nauka_Main {
+
+    public static void main(String[] args){                                 //psvm
+        System.out.println("Hello");                                        //sout
+    }
+}
+
+
+
+
+/**
+ * ctrl + /                     - komentarz linia
+ * ctrl + shift + /             - komentarz blok
+ * ctrl + D                     - kopia całej linii
+ * ctrl + Alt + L               - format kodu
+ * shift + F6                   - refactor - rename
+ * ctrl + K                     - commit (GIT)
+ * ctrl + Shft + K              - push (GIT)
+ */
